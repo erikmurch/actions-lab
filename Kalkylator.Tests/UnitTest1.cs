c987f89e097e28730fd387351bb5609d3f34765b
Erik Murch <erikmurch02@gmail.com>
2026-09-09 15:58:13 +0200

    Skapa kalkylator med tester

---
+++ Kalkylator.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿using Kalkylator;
+public class RaknareTests
+{
+[Fact]
+public void Addera_TvaPlusTre_BlirFem()
+{
+var raknare = new Raknare();
+Assert.Equal(5, raknare.Addera(2, 3));
+}
+}
---
+++ Kalkylator/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Kalkylator;
+public class Raknare
+{
+public int Addera(int a, int b) => a + b;
+}
